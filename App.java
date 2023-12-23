fd2c32b49ec35eba212b4a120c829052c6723f13
annisarasha36 <144711390+annisarasha36@users.noreply.github.com>
2023-12-23 20:39:22 +0700

    Update App.java

--- App.java
+++ App.java
@@ -1,21 +1,14 @@
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.ResourceBundle.Control;
+import java.io.InputStreamReader; // untuk membaca  input pada program saat di panggil 
 
 import controller.ControlPanel;
-import model.Obat;
-import model.Pembeli;
 
 public class App {
-   
+  
     public static void main(String[] args) throws Exception {
         ControlPanel controlKlinik = new ControlPanel();
-        controlKlinik.checkValue();
         controlKlinik.setItem(); // Panggil setItem dari objek controlKlinik
-        controlKlinik.setKuisioner(); //      
+        controlKlinik.setKuisioner(); //    
         boolean lanjut = true;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         controlKlinik.setUser(); // Panggil setUser dari objek controlKlinik
@@ -28,7 +21,10 @@ public class App {
                     controlKlinik.mulaiKuisioner();
                     break;
                 case "2": 
-                    controlKlinik.tampilkanDaftarObat();
+                    controlKlinik.beliObat();
+                    break;
+                case "3": 
+                    controlKlinik.tampilkanLaporan();
                     break;
                 default:
                     lanjut = false;
@@ -37,12 +33,18 @@ public class App {
         }
 
         System.out.println("Sampai Jumpa");
+        System.out.println("Semoga Lekas Sembuh dan Terimakasih :)");
     }
 
     public static void tampilkanMenu() {
-        System.out.println("1. Konsultasi");
-        System.out.println("2. Daftar obat");
-        System.out.println("3. Laporan Penjualan");
-        System.out.println("4. Keluar");
+        System.out.println("+============================================+");
+        System.out.println("| Pilih menu sesuai dengan kebutuhan anda!   |");
+        System.out.println("+============================================+");
+        System.out.println("| 1. Konsultasi                              |");
+        System.out.println("|2. Beli Obat                                |");
+        System.out.println("| 3. Laporan Penjualan                       |");
+        System.out.println("| 4. Keluar                                  |");
+        System.out.println("+============================================+");
     }
 }
+ 
